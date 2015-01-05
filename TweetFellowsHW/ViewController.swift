69faa4ee374df90a0e4b873a8cf9f41e22f253c3
David Rogers <DavidRogers@apples-MacBook-Pro.local>
2015-01-05 13:56:54 -0800

    Initial Commit

---
+++ TweetFellowsHW/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TweetFellowsHW
+//
+//  Created by David Rogers on 1/5/15.
+//  Copyright (c) 2015 David Rogers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
